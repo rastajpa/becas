d91bb1497014f1a4ba8be82f3b51ddd87cefdc66
rastajpa <juanpabloabuin@gmail.com>
2015-06-29 19:56:30 -0300

    paginas agregadas

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,67 +1,78 @@
 angular.module('becas.controllers', ['ionic','ngCordova'])
 .config(function($stateProvider, $urlRouterProvider) {
-
   $stateProvider
-    .state('eventmenu', {
-      url: "/event",
-      abstract: true,
-      templateUrl: "pages/menu.html"
-    })
-    .state('eventmenu.home', {
-      url: "/home",
-      views: {
-        'menuContent' :{
-          templateUrl: "pages/home.html",
-          controller: "homeCtrl"
-        }
+  .state('eventmenu', {
+    url: "/event",
+    abstract: true,
+    templateUrl: "pages/menu.html"
+  })
+  .state('eventmenu.home', {
+    url: "/home",
+    views: {
+      'menuContent' :{
+        templateUrl: "pages/home.html",
+        controller: "homeCtrl"
       }
-    })
-    .state('eventmenu.checkin', {
-      url: "/check-in",
-      views: {
-        'menuContent' :{
-          templateUrl: "pages/check-in.html",
-          controller: "CheckinCtrl"
-        }
+    }
+  })
+  .state('eventmenu.checkin', {
+    url: "/check-in",
+    views: {
+      'menuContent' :{
+        templateUrl: "pages/check-in.html",
+        controller: "CheckinCtrl"
       }
-    })
-    .state('eventmenu.attendees', {
-      url: "/attendees",
-      views: {
-        'menuContent' :{
-          templateUrl: "pages/attendees.html",
-          controller: "AttendeesCtrl"
-        }
+    }
+  })
+  .state('eventmenu.attendees', {
+    url: "/attendees",
+    views: {
+      'menuContent' :{
+        templateUrl: "pages/attendees.html",
+        controller: "AttendeesCtrl"
       }
-    })
-  
+    }
+  })
+  .state('eventmenu.about', {
+    url: "/about",
+    views: {
+      'menuContent' :{
+        templateUrl: "pages/about.html",
+        controller: "aboutCtrl"
+      }
+    }
+  }) 
+  .state('eventmenu.news', {
+    url: "/news",
+    views: {
+      'menuContent' :{
+        templateUrl: "pages/news.html",
+        controller: "newsCtrl"
+      }
+    }
+  })
   $urlRouterProvider.otherwise("/event/home");
 })
 .controller('homeCtrl', function($scope,$ionicSideMenuDelegate) {
-
 })
 .controller('MainCtrl', function($scope, $ionicSideMenuDelegate) {
   $scope.attendees = [
-    { firstname: 'Nicolas', lastname: 'Cage' },
-    { firstname: 'Jean-Claude', lastname: 'Van Damme' },
-    { firstname: 'Keanu', lastname: 'Reeves' },
-    { firstname: 'Steven', lastname: 'Seagal' }
+  { firstname: 'Nicolas', lastname: 'Cage' },
+  { firstname: 'Jean-Claude', lastname: 'Van Damme' },
+  { firstname: 'Keanu', lastname: 'Reeves' },
+  { firstname: 'Steven', lastname: 'Seagal' }
   ];
-  
   $scope.toggleLeft = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
 })
-
 .controller('CheckinCtrl', function($scope) {
-  $scope.showForm = true;
-  
+  $scope.showForm = true;  
   $scope.shirtSizes = [
-    { text: 'Large', value: 'L' },
-    { text: 'Medium', value: 'M' },
-    { text: 'Small', value: 'S' }
+  { text: 'Large', value: 'L' },
+  { text: 'Medium', value: 'M' },
+  { text: 'Small', value: 'S' }
   ];
-  
   $scope.attendee = {};
   $scope.submit = function() {
     if(!$scope.attendee.firstname) {
@@ -71,11 +82,8 @@ angular.module('becas.controllers', ['ionic','ngCordova'])
     $scope.showForm = false;
     $scope.attendees.push($scope.attendee);
   };
-  
 })
-
-.controller('AttendeesCtrl', function($scope) {
-  
+.controller('AttendeesCtrl', function($scope) { 
   $scope.activity = [];
   $scope.arrivedChange = function(attendee) {
     var msg = attendee.firstname + ' ' + attendee.lastname;
@@ -85,6 +93,9 @@ angular.module('becas.controllers', ['ionic','ngCordova'])
     if($scope.activity.length > 3) {
       $scope.activity.splice(0, 1);
     }
-  };
-  
+  };  
+})
+.controller('newsCtrl', function($scope,$ionicSideMenuDelegate) {
+})
+.controller('aboutCtrl', function($scope,$ionicSideMenuDelegate) {
 });
