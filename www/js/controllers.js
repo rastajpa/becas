e097ac3efe387333d358198d2cfc85a6c9c48b52
Gabriel <gabrielbazan7@hotmail.com>
2015-07-28 11:18:30 -0300

    reclamos y estado

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,118 +1,137 @@
-angular.module('becas.controllers', ['ionic','ngCordova'])
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-  .state('eventmenu', {
-    url: "/event",
-    abstract: true,
-    templateUrl: "pages/menu.html",
-  })
-  .state('eventmenu.home', {
-    url: "/home",
-    views: {
-      'menuContent' :{
-        templateUrl: "pages/home.html",
-        controller: "HomeCtrl"
+  angular.module('becas.controllers', ['ionic','ngCordova'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('eventmenu', {
+      url: "/event",
+      abstract: true,
+      templateUrl: "pages/menu.html",
+    })
+    .state('eventmenu.home', {
+      url: "/home",
+      views: {
+        'menuContent' :{
+          templateUrl: "pages/home.html",
+          controller: "HomeCtrl"
+        }
       }
-    }
-  })
-  .state('eventmenu.checkin', {
-    url: "/check-in",
-    views: {
-      'menuContent' :{
-        templateUrl: "pages/check-in.html",
-        controller: "CheckinCtrl"
+    })
+    .state('eventmenu.checkin', {
+      url: "/check-in",
+      views: {
+        'menuContent' :{
+          templateUrl: "pages/check-in.html",
+          controller: "CheckinCtrl"
+        }
       }
-    }
-  })
-  .state('eventmenu.attendees', {
-    url: "/attendees",
-    views: {
-      'menuContent' :{
-        templateUrl: "pages/attendees.html",
-        controller: "AttendeesCtrl"
+    })
+    .state('eventmenu.attendees', {
+      url: "/attendees",
+      views: {
+        'menuContent' :{
+          templateUrl: "pages/attendees.html",
+          controller: "AttendeesCtrl"
+        }
       }
-    }
-  })
-  .state('eventmenu.about', {
-    url: "/about",
-    views: {
-      'menuContent' :{
-        templateUrl: "pages/about.html",
-        controller: "aboutCtrl"
+    })
+    .state('eventmenu.about', {
+      url: "/about",
+      views: {
+        'menuContent' :{
+          templateUrl: "pages/about.html",
+          controller: "aboutCtrl"
+        }
       }
-    }
-  }) 
-  .state('eventmenu.news', {
-    url: "/news",
-    views: {
-      'menuContent' :{
-        templateUrl: "pages/news.html",
-        controller: "newsCtrl"
+    }) 
+    .state('eventmenu.news', {
+      url: "/news",
+      views: {
+        'menuContent' :{
+          templateUrl: "pages/news.html",
+          controller: "newsCtrl"
+        }
       }
-    }
-  })
+    })
 
-  .state('eventmenu.options', {
-    url: "/options",
-    views: {
-      'menuContent' :{
-        templateUrl: "pages/options.html",
-        controller: "optionsCtrl"
+    .state('eventmenu.options', {
+      url: "/options",
+      views: {
+        'menuContent' :{
+          templateUrl: "pages/options.html",
+          controller: "optionsCtrl"
+        }
       }
-    }
-  })
-  .state('eventmenu.options.states', {
-    url: "/states",
-    views: {
-      'state-tab' :{
-        templateUrl: "pages/states.html",
-        controller: "statesCtrl"
+    })
+    .state('eventmenu.options.states1', {
+      url: "/states1",
+      views: {
+        'state-tab' :{
+          templateUrl: "pages/states1.html",
+          controller: "statesCtrl1"
+        }
       }
-    }
-  })
-  .state('eventmenu.options.payments', {
-    url: "/payments",
-    views: {
-      'payments-tab' :{
-        templateUrl: "pages/payments.html",
-        controller: "paymentsCtrl"
+    })
+    .state('eventmenu.options.states2', {
+      url: "/states2/:document",
+      views: {
+        'state-tab' :{
+          templateUrl: "pages/states2.html",
+          controller: "statesCtrl2"
+        }
+      },
+    })
+    .state('eventmenu.options.payments', {
+      url: "/payments",
+      views: {
+        'payments-tab' :{
+          templateUrl: "pages/payments.html",
+          controller: "paymentsCtrl"
+        }
       }
-    }
-  })
-  .state('eventmenu.options.claims', {
-    url: "/claims",
-    views: {
-      'claims-tab' :{
-        templateUrl: "pages/claims.html",
-        controller: "claimsCtrl"
+    })
+    .state('eventmenu.options.claims', {
+      url: "/claims",
+      views: {
+        'claims-tab' :{
+          templateUrl: "pages/claims.html",
+          controller: "claimsCtrl"
+        }
       }
-    }
-  })
-  .state('eventmenu.user', {
-    url: "/user",
-    views: {
-      'menuContent' :{
-        templateUrl: "pages/user.html",
-        controller: "userCtrl"
+    })
+    .state('eventmenu.user', {
+      url: "/user",
+      views: {
+        'menuContent' :{
+          templateUrl: "pages/user.html",
+          controller: "userCtrl"
+        }
       }
-    }
+    })
+    $urlRouterProvider.otherwise("/event/home");
   })
-  $urlRouterProvider.otherwise("/event/home");
-})
-.controller('HomeCtrl', function($scope, LoginService, $ionicPopup, $state,$ionicHistory) {
+  .controller('HomeCtrl', function($scope,$q,$http, LoginService, $ionicPopup, $state,$ionicHistory) {
     $scope.data = {};
     $scope.login = function() {
-        LoginService.loginUser($scope.data.email, $scope.data.password).success(function(data) {
-            $state.go('eventmenu.options');
-        }).error(function(data) {
-            var alertPopup = $ionicPopup.alert({
-                title: 'Falló el ingreso!',
-                template: 'Por favor revise sus datos!'
-            });
+      var session = $q.defer();
+      session.promise.then(userSession);
+      var hola = $http.get('http://localhost/becas/web/usuarios')
+      .success(function(data,status, headers,config){
+      session.resolve(data);
+      })
+      .error(function(data,status,headers,config){
+      });
+      function userSession(data){
+      LoginService.loginUser($scope.data.email, $scope.data.password,data).success(function(data) {
+        $state.go('eventmenu.options.states1');
+      }).error(function(data) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Falló el ingreso!',
+          template: 'Por favor revise sus datos!'
         });
+      });
     }
-})
-.controller('MainCtrl', function($scope, $ionicSideMenuDelegate,$ionicNavBarDelegate,$ionicHistory,$ionicPopover) {
+  }
+  })
+  .controller('MainCtrl', function($scope, $ionicSideMenuDelegate,$ionicNavBarDelegate,$ionicHistory,$ionicPopover) {
 
   // .fromTemplateUrl() method
   $ionicPopover.fromTemplateUrl('pages/popover.html', {
@@ -120,7 +139,7 @@ angular.module('becas.controllers', ['ionic','ngCordova'])
   }).then(function(popover) {
     $scope.popover = popover;
   });
-   $scope.setPlatform = function(p) {
+  $scope.setPlatform = function(p) {
     document.body.classList.remove('platform-ios');
     document.body.classList.remove('platform-android');
     document.body.classList.add('platform-' + p);
@@ -138,131 +157,166 @@ angular.module('becas.controllers', ['ionic','ngCordova'])
   });
   // Execute action on hide popover
   $scope.$on('popover.hidden', function() {
-    // Execute action
-  });
+  // Execute action
+});
   // Execute action on remove popover
   $scope.$on('popover.removed', function() {
-    // Execute action
-  });
+  // Execute action
+});
   $scope.toggleLeft = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
-    $ionicHistory.nextViewOptions({
-  disableAnimate: true,
-  disableBack: true
-});
+  $ionicHistory.nextViewOptions({
+    disableAnimate: true,
+    disableBack: true
+  });
 
   /*$ionicNavBarDelegate.showBackButton();
   $ionicHistory.goBack();*/
 })
-.controller('newsCtrl', function($scope,$ionicSideMenuDelegate) {
-})
-.controller('aboutCtrl', function($scope,$ionicModal,$ionicLoading) {
-  $ionicModal.fromTemplateUrl('pages/map.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-})
+  .controller('newsCtrl', function($scope,$ionicSideMenuDelegate) {
+  })
+  .controller('aboutCtrl', function($scope,$ionicModal,$ionicLoading) {
+    $ionicModal.fromTemplateUrl('pages/map.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    })
 
-  $scope.openModal = function () {
-    $scope.modal.show();
-    var latlng = new google.maps.LatLng(-26.8376638,-65.2127732);
-    var myOptions = {
+    $scope.openModal = function () {
+      $scope.modal.show();
+      var latlng = new google.maps.LatLng(-26.8376638,-65.2127732);
+      var myOptions = {
         zoom: 18,
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      var map = new google.maps.Map(document.getElementById("map"), myOptions);
+      google.maps.event.addDomListener(window, "load");
+      var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: "Secretaría General de Becas"
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+      });
     };
-    var map = new google.maps.Map(document.getElementById("map"), myOptions);
-    google.maps.event.addDomListener(window, "load");
-        var marker = new google.maps.Marker({
-                position: latlng,
-                map: map,
-                title: "Secretaría General de Becas"
-            });
 
-            google.maps.event.addListener(marker, 'click', function() {
-                infowindow.open(map,marker);
-            });
-  };
-   
+
+  })
+
+  .controller('optionsCtrl', function($scope,$ionicHistory,$ionicSlideBoxDelegate) {
+  //  $ionicHistory.nextViewOptions({
+  //   disableAnimate: false,
+  //   disableBack: false
+  // });
 
 })
-
-.controller('optionsCtrl', function($scope,$ionicHistory,$ionicSlideBoxDelegate) {
-//  $ionicHistory.nextViewOptions({
-//   disableAnimate: false,
-//   disableBack: false
-// });
-
+  .controller('claimsCtrl', function($scope,$ionicHistory) {
+    $scope.sendEmail= function() {
+        if(window.plugins && window.plugins.emailComposer) {
+            window.plugins.emailComposer.showEmailComposerWithCallback(function(result) {
+                console.log("Response -> " + result);
+            }, 
+            $scope.subject, // Subject
+            $scope.claims,                      // Body
+            ["juanpabloabuin@gmail.com"],    // To
+            null,                    // CC
+            null,                    // BCC
+            false,                   // isHTML
+            null,                    // Attachments
+            null);                   // Attachment Data
+        }
+    }
 })
-.controller('claimsCtrl', function($scope,$ionicHistory) {
-  /*$ionicHistory.nextViewOptions({
-  disableAnimate: true,
-  disableBack: true
-});*/
-
-})
-.controller('paymentsCtrl', function($scope,$ionicHistory,$q,$http) {
-})
-.controller('statesCtrl', function($scope,$ionicHistory,$q,$http) {
-var session = $q.defer();
-session.promise.then(userSession);
-var hola = $http.get('http://localhost/becas/web/evaluacion')
-.success(function(data,status, headers,config){
-session.resolve(data);
-})
-.error(function(data,status,headers,config){
-});
-function userSession(data){
-  //hay que hacer control para el documento
-  if(data[1].causa1== null && data[1].causa2== null &&
-  data[1].causa3== null && data[1].causa4== null &&
-  data[1].comentarioE== ''){
+  .controller('paymentsCtrl', function($scope,$ionicHistory,$q,$http) {
+  })
+  .controller('statesCtrl1', function($scope,$ionicHistory,StateService,$q,$http,$state,$ionicPopup) {
+    $scope.data = {};
+    $scope.state = function() {
+      var session = $q.defer();
+      session.promise.then(userSession);
+      var hola = $http.get('http://localhost/becas/web/alumnos')
+      .success(function(data,status, headers,config){
+      session.resolve(data);
+      })
+      .error(function(data,status,headers,config){
+      });
+      function userSession(data){
+      StateService.UserState($scope.data.document,data).success(function(data) {
+        $state.go('eventmenu.options.states2', {document: $scope.data.document});
+      }).error(function(data) {
+        var alertPopup = $ionicPopup.alert({
+          title: 'Ingreso de manera incorrecta su documento!',
+          template: 'Por favor revise sus datos!'
+        });
+      });
+    }
+  }
+  })
+  .controller('statesCtrl2', function($scope,$ionicHistory,StateService,$q,$http,$stateParams) {
     var session = $q.defer();
-    session.promise.then(userSession2);
-    var hola = $http.get('http://localhost/becas/web/alumnos')
-.success(function(data,status, headers,config){
-session.resolve(data);
-})
-.error(function(data,status,headers,config){
-});
+    session.promise.then(userSession);
+    var hola = $http.get('http://localhost/becas/web/evaluacion')
+    .success(function(data,status, headers,config){
+      session.resolve(data);
+    })
+    .error(function(data,status,headers,config){
+    });
+    function userSession(data){
+      for (var i = data.length - 1; i >= 0; i--) {
+        if(data[i].dniE==$stateParams.document){
+  if(data[i].causa1== null && data[i].causa2== null &&
+    data[i].causa3== null && data[i].causa4== null &&
+    data[i].comentarioE== ''){
+    var session = $q.defer();
+  session.promise.then(userSession2);
+  var hola = $http.get('http://localhost/becas/web/alumnos')
+  .success(function(data,status, headers,config){
+    session.resolve(data);
+  })
+  .error(function(data,status,headers,config){
+  });
   function userSession2(data2){
-  if(data2[0].becario==1){
-    console.log("hola");
-    //muestra RENOVANTE
-    $scope.state = "RENOVANTE";
-    //muestra PUNTAJE
-    $scope.score = "Entero que muestra el puntaje";
-  }
-  if(data2[0].becario==0){
-    //muestra APROBADO
-     $scope.state = "APROBADO";
-    //muestra PUNTAJE
-    $scope.score = "Entero que muestra el puntaje";
-  }
-  }
-  }
-  else{
+    if(data2[i].becario==1){
+  //muestra RENOVANTE
+  $scope.state = "RENOVANTE";
+  //muestra PUNTAJE
+  $scope.score = "Entero que muestra el puntaje";
+}
+if(data2[i].becario==0){
+  //muestra APROBADO
+  $scope.state = "APROBADO";
+  //muestra PUNTAJE
+  $scope.score = "Entero que muestra el puntaje";
+}
+}
+}
+else{
   //muestra FUERA DE CONCURSO
   $scope.state = "FUERA DE CONCURSO";
   //muestra causa1
-  $scope.cause1 = data[1].causa1;
+  $scope.cause1 = data[i].causa1;
   //muestra causa2
-  $scope.cause1 = data[1].causa2;
+  $scope.cause1 = data[i].causa2;
   //muestra causa3
-    $scope.cause1 = data[1].causa3;
+  $scope.cause1 = data[i].causa3;
   //muestra causa4
-    $scope.cause1 = data[1].causa4;
+  $scope.cause1 = data[i].causa4;
   //muestra data[1].comentarioE
-    $scope.commentE = data[1].comentarioE;
-  }
+  $scope.commentE = data[i].comentarioE;
+}
+}
+}
 }
 })
-.controller('userCtrl', function($scope,$ionicHistory,$ionicNavBarDelegate) {
-  console.log("hola");
- $ionicNavBarDelegate.showBackButton();
-  $scope.myGoBack = function() {
-    $ionicHistory.goBack();
-  };
-});
+  .controller('userCtrl', function($scope,$ionicHistory,$ionicNavBarDelegate) {
+    console.log("hola");
+    $ionicNavBarDelegate.showBackButton();
+    $scope.myGoBack = function() {
+      $ionicHistory.goBack();
+    };
+  });
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,12 +1,17 @@
 angular.module('becas.services', ['ionic','ngCordova'])
 .service('LoginService', function($q) {
     return {
-        loginUser: function(name, pw) {
+        loginUser: function(email, pw, data) {
             var deferred = $q.defer();
             var promise = deferred.promise;
- 
-            if (name == 'user' && pw == 'secret') {
-                deferred.resolve('Bienvenido ' + name + '!');
+            for (var i = data.length - 1; i >= 0; i--) { 
+            if (email == data[i].email && pw == data[i].clave){
+                var email1 = data[i].email;
+                var clave1 = data[i].clave;
+            } 
+    
+            if (email == email1 && pw == clave1) {
+                deferred.resolve('Bienvenido!');
             } else {
                 deferred.reject('Datos erroneos.');
             }
@@ -21,4 +26,32 @@ angular.module('becas.services', ['ionic','ngCordova'])
             return promise;
         }
     }
+}
 })
+.service('StateService', function($q){
+    return {
+        UserState: function(doc,data){
+           var deferred = $q.defer();
+            var promise = deferred.promise;  
+            for (var i = data.length - 1; i >= 0; i--) {
+                if (doc == data[i].dni) {
+                var dni= data[i].dni
+            }
+            }
+            if (doc == dni) {
+                deferred.resolve('Cualquier duda que tenga, por favor escribanos un mail en la sección de reclamos');
+            } else {
+                deferred.reject('Datos erroneos.');
+            }
+            promise.success = function(fn) {
+                promise.then(fn);
+                return promise;
+            }
+            promise.error = function(fn) {
+                promise.then(null, fn);
+                return promise;
+            }
+            return promise;
+        }
+    }
+});
