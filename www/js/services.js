e3b2e5e02f0c3b3ddba640376bc5c9553b9d480f
Gabriel <gabrielbazan7@hotmail.com>
2015-08-05 21:11:43 -0300

    ...

--- www/js/services.js
+++ www/js/services.js
@@ -32,7 +32,7 @@ app.service('loginServices', ['$q','$http','$ionicPopup','$state', '$ionicLoadin
             else{
                 $ionicLoading.hide();
                 loginPut(data);
-                $state.go('eventmenu.options.states');
+                $state.go('eventmenu.options');
             }
         }
     };
@@ -52,10 +52,10 @@ app.service('loginServices', ['$q','$http','$ionicPopup','$state', '$ionicLoadin
 }]);
 
 app.service('evaluacionServices', ['$q','$http','$ionicPopup','$state', '$ionicLoading', function ($q,$http,$ionicPopup,$state,$ionicLoading){
-    var causa1 = "";
-    var causa2 = "";
-    var causa3 = "";
-    var causa4 = "";
+    var causa1 = null;
+    var causa2 = null;
+    var causa3 = null;
+    var causa4 = null;
     var comentarioE = "";
 
     this.evaluacion = function(dni){
@@ -100,7 +100,6 @@ app.service('alumnosServices', ['$q','$http','$ionicPopup','$state', '$ionicLoad
     var idcarrera = "";
 
     this.alumnos = function(dni){
-        console.log(dni);
         var session = $q.defer();
         session.promise.then(alumnosSession);
         var hola = $http.get('http://192.168.1.110/becas/web/alumnos?AlumnosSearch[dni]=' + dni)
